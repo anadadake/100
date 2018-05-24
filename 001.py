6dd74a46eed7f96bb4e67afbf91b1fc4ea396e60
anadadake <anadadake@hotmail.com>
2018-05-24 21:13:54 +0800

    final

--- 001.py
+++ 001.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+# if __name__ == '__main__':
+#     for i in range(1, 5):
+#         for j in range(1, 5):
+#             for k in range(1, 5):
+#                 if (i != k) and (i != j) and (j != k):
+#                     print(i, j, k)
+
+
 if __name__ == '__main__':
     for i in range(1, 5):
         for j in range(1, 5):
             for k in range(1, 5):
                 if (i != k) and (i != j) and (j != k):
-                    print(i, j, k)
+                    print(str(i)+str(j)+str(k))
