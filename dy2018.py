f4e91555b74c278016ba11105d55ee43fac3b208
Kevin Ma <anadadake@hotmail.com>
2018-06-29 17:38:13 +0800

    dy2018 magnet url downloader

--- dy2018.py
+++ dy2018.py
@@ -7,10 +7,9 @@ import datetime
 # Step 1: 处理index页面，获取全部2级栏位和对应的页面url
 # Step 2：循环2级栏位，循环页面，获取电影名称和下载的url，保存成csv
 
-
 index_url = 'https://www.dy2018.com'
-now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
+now_time = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
+csv_file_name= ''.join([now_time, '_csv' ,'.txt'])
 
 def process_magnet_link(url, title):
     sub_response = urllib.request.urlopen(url)
@@ -19,36 +18,26 @@ def process_magnet_link(url, title):
     if magnet_url != None and magnet_url.text != None and title != None:
         return magnet_url.text + ',' + title
     else:
-        return 'error:' + url + "," + title
-
+        return 'error: no valid magnet url in ' + url + "," + title
 
 if __name__ == '__main__':
+    print('Author Kevin on 2018-06-29.')
+    print('Starting to get movie magnet url from https://www.dy2018.com ...')
+    # print(csv_file_name)
+    # print(type(csv_file_name))
     response = urllib.request.urlopen(index_url)
     index_html = response.read()
 
     soup = BeautifulSoup(index_html, 'html.parser')
-
-    # second
-
     sub_htmls = soup.find_all('a', href=re.compile('^/i/\d*\.html'))
-
-    with open('output.csv', 'w', encoding='utf-8') as output_file:
+    with open(csv_file_name, 'w', encoding='utf-8') as output_file:
         for i in sub_htmls:
-            # print(i)
-            # print(index_url + i.attrs.get('href'))
-            # print(i.attrs.get('title'))
             sub_url = index_url + i.attrs.get('href')
             title = i.attrs.get('title')
             dl_link_with_title = process_magnet_link(sub_url, title)
             print(dl_link_with_title)
-            output_file.writelines(index_url + i.attrs.get('href') + ',' + i.attrs.get('title') + '\n')
+            #output_file.writelines(index_url + i.attrs.get('href') + ',' + i.attrs.get('title') + '\n')
+            output_file.writelines(dl_link_with_title + '\n')
 
-        # print(i.find())
-        # sub_html = index_url+ Stri.href
-        # print(i.href)
-
-    # secondParentList = soup.find_all(class_='co_area2')
-    # for second in secondParentList:
-    #     print(second.a.text)
-
-    # print(soup.prettify())
+    print(''.join(['Finshed. Please refer to file ',csv_file_name]))
+    print(' :) orz. Enjoy the movie.')
